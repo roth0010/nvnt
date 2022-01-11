42361fef10dafc5b4cd6b4bfdb1d69f2d6ca5a1e
roth0010 <89872836+roth0010@users.noreply.github.com>
2022-01-11 14:18:58 +0100

    Stable Version (I Hope)

--- build/Level1.js
+++ build/Level1.js
@@ -17,22 +17,14 @@ export default class Level1 extends Level {
     render(ctx, canvas) {
         const image = Static.loadNewImage('./assets/img/levelonebackground.png');
         ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-<<<<<<< HEAD
         Static.writeTextToCanvas(canvas, 'Press E to open phone', canvas.width / 2, 50, 30, 'black');
-=======
-        Static.writeTextToCanvas(canvas, 'Press E to open your phone', canvas.width / 2, 50, 30, 'black');
-        this.cat.render(ctx, canvas);
->>>>>>> 6d4c2a8a4d582e22c9a172e50dbb742747aea19f
         const image2 = Static.loadNewImage(this.game.getMonsterType());
         image2.height = canvas.height / 2;
         image2.width = (canvas.height / 2) * Static.getMonsterAR(this.game.getMonsterType());
         ctx.drawImage(image2, ((canvas.width / 2) - (image2.width / 2)), (canvas.height / 2 - (image2.width / 2)), image2.width, image2.height);
         const monsterName = this.game.getMonsterName();
         Static.writeTextToCanvas(canvas, monsterName, canvas.width / 2, 110, 60, 'black');
-<<<<<<< HEAD
         this.cat.render(ctx, canvas);
-=======
->>>>>>> 6d4c2a8a4d582e22c9a172e50dbb742747aea19f
     }
 }
 //# sourceMappingURL=Level1.js.map
--- src/Level1.ts
+++ src/Level1.ts
@@ -44,7 +44,6 @@ export default class Level1 extends Level {
   public render(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement): void {
     const image = Static.loadNewImage('./assets/img/levelonebackground.png');
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-<<<<<<< HEAD
     Static.writeTextToCanvas(canvas, 'Press E to open phone', canvas.width / 2, 50, 30, 'black');
     const image2 = Static.loadNewImage(this.game.getMonsterType());
     image2.height = canvas.height / 2;
@@ -63,14 +62,5 @@ export default class Level1 extends Level {
     const monsterName = this.game.getMonsterName();
     Static.writeTextToCanvas(canvas, monsterName, canvas.width / 2, 110, 60, 'black');
     this.cat.render(ctx, canvas);
-=======
-
-    Static.writeTextToCanvas(canvas, 'Press E to open your phone', canvas.width / 2, 50, 30, 'black');
-    this.cat.render(ctx, canvas);
-    const image2 = Static.loadNewImage(this.game.getMonsterType());
-    ctx.drawImage(image2, 50, 0, canvas.width, canvas.height);
-    const monsterName = this.game.getMonsterName(); // these two lines put the user typed monster on screen, this isn't necessary but it was to test if this screen can acess the name
-    Static.writeTextToCanvas(canvas, monsterName, canvas.width / 2, 110, 60, 'black');
->>>>>>> 6d4c2a8a4d582e22c9a172e50dbb742747aea19f
   }
 }
